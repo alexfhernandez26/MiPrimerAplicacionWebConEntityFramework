369096950c3e8c37b1aa7a4f6c4c14a2535d0abe
Alex26 <alexfhernandez26@gmail.com>
2021-07-03 20:38:08 -0400

    Se creo el proyecto con la clase marca para empezar

---
+++ MiPrimerAplicacionWebConEntityFramework/Models/MarcaCLS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class MarcaCLS
+    {
+        [Display(Name ="ID Marca")]
+        [Key]
+        public int iidmarca { get; set; }
+        [Display(Name = "Nombre")]
+        public string nombre { get; set; }
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+        public int bhabilitado { get; set; }
+
+    }
+}
