cb74dce62651294c5a8a9aec39d0d13dc58831d6
Alex26 <alexfhernandez26@gmail.com>
2021-07-03 22:08:29 -0400

    Se agrego el modelo,vista y controlador de cliente

---
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/ClienteController.cs
@@ -0,0 +1,32 @@
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Controllers
+{
+    public class ClienteController : Controller
+    {
+        // GET: Cliente
+        List<ClienteCLS> listaCliente = null;
+        public ActionResult Index()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listaCliente = (from cliente in bd.Cliente
+                              select new ClienteCLS
+                              {
+                                  iidCliente = cliente.IIDCLIENTE,
+                                  NOMBRE = cliente.NOMBRE,
+                                  APPATERNO = cliente.APPATERNO,
+                                  APMATERNO = cliente.APMATERNO,
+                                  TELEFONOCELULAR = cliente.TELEFONOCELULAR
+                              }).ToList();
+            }
+
+            return View(listaCliente);
+        }
+    }
+}
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/ClienteCLS.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class ClienteCLS
+    {
+        [Key]
+        [Display(Name ="Id Cliente")]
+        public int iidCliente { get; set; }
+        [Display(Name = "Nombre")]
+        public string NOMBRE { get; set; }
+        [Display(Name = "Apellido")]
+        public string APPATERNO { get; set; }
+        [Display(Name = "Segundo apellido")]
+        public string APMATERNO { get; set; }
+        [Display(Name = "Correo")]
+        public string EMAIL { get; set; }
+        [Display(Name = "Direccion")]
+        public string DIRECCION { get; set; }
+        [Display(Name = "Sexo")]
+        public int IIDSEXO { get; set; }
+        [Display(Name = "Telefono")]
+        public string TELEFONOFIJO { get; set; }
+        [Display(Name = "Celular")]
+        public string TELEFONOCELULAR { get; set; }
+        public int BHABILITADO { get; set; }
+      
+
+    }
+}
