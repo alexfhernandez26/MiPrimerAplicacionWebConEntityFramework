6a1c62ee184764fc05966297a0bf48a2d4c3d539
Alex26 <alexfhernandez26@gmail.com>
2021-07-03 21:38:52 -0400

    Se agrego el modelo,vista y controlador de sucursal

---
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/SucursalController.cs
@@ -0,0 +1,31 @@
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Controllers
+{
+    public class SucursalController : Controller
+    {
+        // GET: Sucursal
+        List<SucursalCLS> listaSucursal = null;
+        public ActionResult Index()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listaSucursal = (from sucursal in bd.Sucursal
+                                 where sucursal.BHABILITADO == 1
+                                 select new SucursalCLS
+                                 {
+                                     iidsucursal = sucursal.IIDSUCURSAL,
+                                     nombre = sucursal.NOMBRE,
+                                     telefono =sucursal.TELEFONO,
+                                     email =sucursal.EMAIL
+                                 }).ToList();
+            }
+                return View(listaSucursal);
+        }
+    }
+}
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/SucursalCLS.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class SucursalCLS
+    {
+        [Key]
+        public int iidsucursal { get; set; }
+        [Display(Name ="Nombre")]
+        public string nombre { get; set; }
+        [Display(Name = "Direccion")]
+        public string direccion { get; set; }
+        [Display(Name = "Telefono")]
+        public string telefono { get; set; }
+        [Display(Name = "Email")]
+        public string email { get; set; }
+        [Display(Name = "Fecha Apertura")]
+        public DateTime fechaApertura { get; set; }
+        public int bhabilitado { get; set; }
+
+    }
+}
