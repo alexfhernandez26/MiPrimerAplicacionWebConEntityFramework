97e5df19aa6813812ca79a505e583d6cac06771c
Alex26 <alexfhernandez26@gmail.com>
2021-07-09 22:08:00 -0400

    Como llenar comboBox y se creo el agregar de clientes, en el mismo es que se utiliza el comboBox

--- MiPrimerAplicacionWebConEntityFramework/Controllers/ClienteController.cs
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/ClienteController.cs
@@ -16,7 +16,8 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
             using (var bd = new BDPasajeEntities())
             {
                 listaCliente = (from cliente in bd.Cliente
-                              select new ClienteCLS
+                                where cliente.BHABILITADO ==1
+                              select new ClienteCLS                             
                               {
                                   iidCliente = cliente.IIDCLIENTE,
                                   NOMBRE = cliente.NOMBRE,
@@ -29,8 +30,30 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
             return View(listaCliente);
         }
 
+        //Llenado comboBox,para llenar un comboBox c# pide que le des una lista de SelectListItem
+        List<SelectListItem> listaSexo = null;
+        public void llenarComboSexo()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listaSexo = (from sexo in bd.Sexo
+                             where sexo.BHABILITADO == 1
+                             select new SelectListItem
+                             {
+                                 Text = sexo.NOMBRE,
+                                 Value = sexo.IIDSEXO.ToString()
+                             }).ToList();
+
+                listaSexo.Insert(0,new SelectListItem {Text="--Seleccione--",Value="" });
+            }
+        }
+
         public ActionResult Agregar()
         {
+            //viewBag me permite pasar informacion del controller a la vista
+
+            llenarComboSexo();
+            ViewBag.lista = listaSexo;
             return View();
         }
 
@@ -40,13 +63,26 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
 
             if (!ModelState.IsValid)
             {
+                llenarComboSexo();
+                ViewBag.lista = listaSexo;
                 return View(clienteCLS);
             }
             else
             {
                 using (var bd = new BDPasajeEntities())
                 {
-                    
+                    Cliente cliente = new Cliente();
+                    cliente.NOMBRE = clienteCLS.NOMBRE;
+                    cliente.APPATERNO = clienteCLS.APPATERNO;
+                    cliente.APMATERNO = clienteCLS.APMATERNO;
+                    cliente.EMAIL = clienteCLS.EMAIL;
+                    cliente.DIRECCION = clienteCLS.DIRECCION;
+                    cliente.IIDSEXO = clienteCLS.IIDSEXO;
+                    cliente.TELEFONOFIJO = clienteCLS.TELEFONOFIJO;
+                    cliente.TELEFONOCELULAR = clienteCLS.TELEFONOCELULAR;
+                    cliente.BHABILITADO = 1;
+                    bd.Cliente.Add(cliente);
+                    bd.SaveChanges();
                 }
             }
 
