b2020cf15169438e8fe7e4fa034008e7898b785a
Alex26 <alexfhernandez26@gmail.com>
2021-07-11 18:56:14 -0400

    Join multi tablas,se creo la clase empleado y se realizo un join multitabla

---
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/EmpleadoController.cs
@@ -0,0 +1,36 @@
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Controllers
+{
+    public class EmpleadoController : Controller
+    {
+        // GET: Empleado
+        List<EmpleadoCLS> listaEmpelados = null;
+        public ActionResult Index()
+        {
+            using (var bd = new BDPasajeEntities())
+            {
+                listaEmpelados = (from empleados in bd.Empleado
+                                  join tipousuario in bd.TipoUsuario
+                                  on empleados.IIDTIPOUSUARIO equals tipousuario.IIDTIPOUSUARIO
+                                  join tipocontrato in bd.TipoContrato
+                                  on empleados.IIDTIPOCONTRATO equals tipocontrato.IIDTIPOCONTRATO
+                                  select new EmpleadoCLS
+                                  {
+                                      iidempleado = empleados.IIDEMPLEADO,
+                                      nombre = empleados.NOMBRE,
+                                      apPaterno = empleados.APPATERNO,
+                                      NombreTipoUsuario = tipousuario.NOMBRE,
+                                      NombreTipoContrato = tipocontrato.NOMBRE
+                                  }).ToList();
+            }
+               
+            return View(listaEmpelados);
+        }
+    }
+}
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/EmpleadoCLS.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class EmpleadoCLS
+    {
+        [Key]
+        public int iidempleado { get; set; }
+        [Required]
+        [Display(Name ="Nombre")]
+        public string nombre { get; set; }
+        [Required]
+        [Display(Name = "Apellido Paterno")]
+        public string apPaterno { get; set; }
+        [Required]
+        [Display(Name = "Apellido Materno")]
+        public string apMaterno { get; set; }
+        [Required]
+        [Display(Name = "Fecha Contrato")]
+        public DateTime fechaContrato { get; set; }
+        [Required]
+        [Display(Name = "Tipo Usuario")]
+        public int iidtipousuario { get; set; }
+        [Required]
+        [Display(Name = "Tipo Contrato")]
+        public int iidtipocontrato { get; set; }
+        [Required]
+        [Display(Name = "Sexo")]
+        public int iidsexo { get; set; }
+
+        public int bhabilitado { get; set; }
+
+        //Propiedades adicionales
+        [Display(Name = "TipoUsuario")]
+        public string NombreTipoUsuario { get; set; }
+
+        [Display(Name = "TipoContrato")]
+        public string NombreTipoContrato { get; set; }
+
+
+    }
+}
