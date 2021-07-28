7a41df621c1f6d48b1b6da69589532a7a467465c
Alex26 <alexfhernandez26@gmail.com>
2021-07-27 22:16:26 -0400

    se creo el metodo editar en marca con su vista,controlador

--- MiPrimerAplicacionWebConEntityFramework/Controllers/MarcaController.cs
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/MarcaController.cs
@@ -30,6 +30,41 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
         {
             return View();
         }
+
+        public ActionResult Editar( int id)
+        {
+            MarcaCLS marcaCLS = new MarcaCLS();
+
+            using (var bd = new BDPasajeEntities())
+            {
+                Marca marca = bd.Marca.Where(p => p.IIDMARCA.Equals(id)).First();
+                marcaCLS.iidmarca = marca.IIDMARCA;
+                marcaCLS.nombre = marca.NOMBRE;
+                marcaCLS.descripcion= marca.DESCRIPCION;
+            }
+
+                return View(marcaCLS);
+        }
+        [HttpPost]
+        public ActionResult Editar(MarcaCLS marcaCLS)
+        {
+            Marca marca = new Marca();
+
+            if (!ModelState.IsValid)
+            {
+                return View(marcaCLS);
+            }
+            using (var bd = new BDPasajeEntities())
+            {
+                marca = bd.Marca.Where(p => p.IIDMARCA.Equals(marcaCLS.iidmarca)).First();
+                marca.NOMBRE = marcaCLS.nombre;
+                marca.DESCRIPCION = marcaCLS.descripcion;
+                bd.SaveChanges();
+            }
+
+            return RedirectToAction("Index");
+        }
+
         [HttpPost]
         public ActionResult Agregar(MarcaCLS marcaCLS)
         {
