6d4eda499d20384f9a8ce0e05c6038915b0dc2ca
Alex26 <alexfhernandez26@gmail.com>
2021-08-25 22:43:22 -0400

    se agregaron varias clases y controladores como Pagina,Rol,TipoUsuario y usuario,tambien instalamos la libreria jqueryunoptusive

---
+++ MiPrimerAplicacionWebConEntityFramework/Models/PaginaCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class PaginaCLS
+    {
+        public int iidpagina { get; set; }
+        [Required]
+        [Display(Name = "Mensaje")]
+        public string mensaje { get; set; }
+        [Required]
+        [Display(Name = "Accion")]
+        public string accion { get; set; }
+        [Required]
+        [Display(Name = "Controlador")]
+        public string controlador { get; set; }
+
+        public int bhabilitado { get; set; }
+    }
+}
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/RolCLS.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class RolCLS
+    {
+        public int iidrol { get; set; }
+        [Required]
+        [Display(Name ="Nombre")]
+        public string nombre { get; set; }
+        [Required]
+        [Display(Name = "Descripcion")]
+        public string descripcion { get; set; }
+        public int bhabilitado { get; set; }
+
+    }
+}
