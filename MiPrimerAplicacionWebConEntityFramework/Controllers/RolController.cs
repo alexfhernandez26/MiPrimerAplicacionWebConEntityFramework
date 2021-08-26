8213340ee5f8cfe26a2202dac5c2f04972b8cd01
Alex26 <alexfhernandez26@gmail.com>
2021-08-25 23:47:42 -0400

    se agrego una busqueda en rol mediante ajax, la busqueda es asincrona donde podemos buscar en el index sin recargar la pagina

--- MiPrimerAplicacionWebConEntityFramework/Controllers/RolController.cs
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/RolController.cs
@@ -26,5 +26,39 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
             }
                 return View(listaRol);
         }
+
+        public ActionResult Filtro(string nombre)
+        {
+            List<RolCLS> listaRol = null;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                if (nombre == null)
+                {
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                select new RolCLS
+                                {
+                                    iidrol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+                                }).ToList();
+                }
+                else
+                {
+                    listaRol = (from rol in bd.Rol
+                                where rol.BHABILITADO == 1
+                                && rol.NOMBRE.Contains(nombre)
+                                select new RolCLS
+                                {
+                                    iidrol = rol.IIDROL,
+                                    nombre = rol.NOMBRE,
+                                    descripcion = rol.DESCRIPCION
+                                }).ToList();
+                }
+               
+            }
+            return PartialView("_tablaRol",listaRol);
+        }
     }
 }
