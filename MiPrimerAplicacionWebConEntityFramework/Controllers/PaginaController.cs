cda40dbed2eeaf322027a74670d522cf46309814
Alex26 <alexfhernandez26@gmail.com>
2021-08-29 19:22:45 -0400

    se agrego un filtrado asincrono con ajax a Pagina

--- MiPrimerAplicacionWebConEntityFramework/Controllers/PaginaController.cs
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/PaginaController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +12,58 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
         // GET: Pagina
         public ActionResult Index()
         {
-            return View();
+            List<PaginaCLS> listaPagina = null;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                listaPagina = (from pagina in bd.Pagina
+                               where pagina.BHABILITADO == 1
+                               select new PaginaCLS
+                               {
+                                   iidpagina= pagina.IIDPAGINA,
+                                   mensaje = pagina.MENSAJE,
+                                   accion = pagina.ACCION,
+                                   controlador = pagina.CONTROLADOR
+                               }).ToList();
+            }
+                return View(listaPagina);
+        }
+
+        public ActionResult Filtrar(PaginaCLS paginaCLS)
+        {
+            List<PaginaCLS> listaPagina = null;
+
+            using (var bd = new BDPasajeEntities())
+            {
+                if (paginaCLS.mensaje ==null)
+                {
+                    listaPagina = (from pagina in bd.Pagina
+                                   where pagina.BHABILITADO == 1
+                                   select new PaginaCLS
+                                   {
+                                       iidpagina = pagina.IIDPAGINA,
+                                       mensaje = pagina.MENSAJE,
+                                       accion = pagina.ACCION,
+                                       controlador = pagina.CONTROLADOR
+                                   }).ToList();
+
+                }
+                else
+                {
+                    listaPagina = (from pagina in bd.Pagina
+                                   where pagina.BHABILITADO == 1
+                                   && pagina.MENSAJE.Contains(paginaCLS.mensaje)
+                                   select new PaginaCLS
+                                   {
+                                       iidpagina = pagina.IIDPAGINA,
+                                       mensaje = pagina.MENSAJE,
+                                       accion = pagina.ACCION,
+                                       controlador = pagina.CONTROLADOR
+                                   }).ToList();
+                }
+                
+            }
+            return PartialView("_tablaPagina",listaPagina);
         }
     }
 }
