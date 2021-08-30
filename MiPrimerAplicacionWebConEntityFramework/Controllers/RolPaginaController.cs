7f94524354f39276b2a182e99a835347f81b0802
Alex26 <alexfhernandez26@gmail.com>
2021-08-29 22:51:29 -0400

    se agrego un filtro sensitivo usando combobox con ajax en rol pagina

---
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/RolPaginaController.cs
@@ -0,0 +1,92 @@
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Controllers
+{
+    public class RolPaginaController : Controller
+    {
+        // GET: RolPagina
+        public ActionResult Index()
+        {
+            llenarRolPagina();
+            List<RolPaginaCLS> listaRpagina = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                listaRpagina = (from rolpagina in bd.RolPagina
+                                join rol in bd.Rol
+                                on rolpagina.IIDROL equals rol.IIDROL
+                                join pagina in bd.Pagina
+                                on rolpagina.IIDPAGINA equals pagina.IIDPAGINA
+                                where rolpagina.BHABILITADO == 1
+                                select new RolPaginaCLS
+                                {
+                                    iidrolpagina = rolpagina.IIDROLPAGINA,
+                                    nombrerol= rol.NOMBRE,
+                                     nombrepagina = pagina.MENSAJE
+                                }).ToList();
+            }
+                return View(listaRpagina);
+        }
+
+        public ActionResult Filtrar(RolPaginaCLS rolPaginaCLS)
+        {
+            List<RolPaginaCLS> listaRpagina = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                
+                if (rolPaginaCLS.iidrol == 0)
+                {
+                    listaRpagina = (from rolpagina in bd.RolPagina
+                                    join rol in bd.Rol
+                                    on rolpagina.IIDROL equals rol.IIDROL
+                                    join pagina in bd.Pagina
+                                    on rolpagina.IIDPAGINA equals pagina.IIDPAGINA
+                                    where rolpagina.BHABILITADO == 1
+                                    select new RolPaginaCLS
+                                    {
+                                        iidrolpagina = rolpagina.IIDROLPAGINA,
+                                        nombrerol = rol.NOMBRE,
+                                        nombrepagina = pagina.MENSAJE
+                                    }).ToList();
+                }
+                else
+                {
+                    listaRpagina = (from rolpagina in bd.RolPagina
+                                    join rol in bd.Rol
+                                    on rolpagina.IIDROL equals rol.IIDROL
+                                    join pagina in bd.Pagina
+                                    on rolpagina.IIDPAGINA equals pagina.IIDPAGINA
+                                    where rolpagina.BHABILITADO == 1
+                                    && rol.IIDROL.Equals(rolPaginaCLS.iidrol)
+                                    select new RolPaginaCLS
+                                    {
+                                        iidrolpagina = rolpagina.IIDROLPAGINA,
+                                        nombrerol = rol.NOMBRE,
+                                        nombrepagina = pagina.MENSAJE
+                                    }).ToList();
+                }
+            }
+                
+            return PartialView("_tablaRolPagina",listaRpagina);
+        }
+        public void llenarRolPagina()
+        {
+            List<SelectListItem> listarolP = null;
+            using (var bd = new BDPasajeEntities())
+            {
+                listarolP = (from rol in bd.Rol
+                             select new SelectListItem
+                             {
+                                 Text = rol.NOMBRE,
+                                 Value = rol.IIDROL.ToString()
+                             }).ToList();
+                listarolP.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listaRol = listarolP;
+            }
+        }
+    }
+}
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/RolPaginaCLS.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class RolPaginaCLS
+    {
+        [Display(Name ="ID RolPagina")]
+        public int iidrolpagina { get; set; }
+        public int iidrol { get; set; }
+        public int iidpagina { get; set; }
+        public int bhabilitado { get; set; }
+
+        //Propiedades adicionales
+        [Display(Name = "Nombre Rol")]
+        public string nombrerol{ get; set; }
+        [Display(Name = "Nombre Pagina")]
+        public string nombrepagina { get; set; }
+
+    }
+}
