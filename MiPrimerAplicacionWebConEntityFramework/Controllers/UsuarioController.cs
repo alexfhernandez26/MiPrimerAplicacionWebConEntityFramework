19a2c6c08d6b541faa3f1731d10bfc8aeb2f303b
Alex26 <alexfhernandez26@gmail.com>
2021-09-08 19:29:22 -0400

    Se agrego el agregar de usuario aqui tenemos un agregar usuario que une clietes y empleados para agregar como usuario

--- MiPrimerAplicacionWebConEntityFramework/Controllers/UsuarioController.cs
+++ MiPrimerAplicacionWebConEntityFramework/Controllers/UsuarioController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MiPrimerAplicacionWebConEntityFramework.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,9 +10,68 @@ namespace MiPrimerAplicacionWebConEntityFramework.Controllers
     public class UsuarioController : Controller
     {
         // GET: Usuario
+
+
         public ActionResult Index()
         {
+            ListaPersonas();
+            listaRol();
             return View();
         }
+
+        public void ListaPersonas()
+        {
+            List<SelectListItem> listaPersonas = new List<SelectListItem>();
+            using (var bd = new BDPasajeEntities())
+            {
+                
+
+                List<SelectListItem> listaCliente = (from cliente in bd.Cliente
+                                                     where cliente.BHABILITADO == 1 && cliente.bTieneUsuario != 1
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = cliente.NOMBRE + " " + cliente.APPATERNO + "(C)",
+                                                         Value = cliente.IIDCLIENTE.ToString()
+                                                     }).ToList();
+
+                List<SelectListItem> listaEmpleado = (from empleado in bd.Empleado
+                                                      where empleado.BHABILITADO == 1 && empleado.bTieneUsuario != 1
+                                                      select new SelectListItem
+                                                      {
+                                                          Text = empleado.NOMBRE + " " + empleado.APPATERNO + "(E)",
+                                                          Value = empleado.IIDEMPLEADO.ToString()
+                                                      }).ToList();
+
+                listaPersonas.AddRange(listaCliente);
+                listaPersonas.AddRange(listaEmpleado);
+
+                listaPersonas.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listapersonas = listaPersonas;
+            }          
+        }
+
+        public void listaRol()
+        {
+           
+            using (var bd = new BDPasajeEntities())
+            {
+
+
+                List<SelectListItem> listaRol = (from rol in bd.Rol
+                                                     where rol.BHABILITADO == 1 
+                                                     select new SelectListItem
+                                                     {
+                                                         Text = rol.NOMBRE,
+                                                         Value =rol.IIDROL.ToString()
+                                                     }).ToList();
+
+                listaRol.Insert(0, new SelectListItem { Text = "--Seleccione", Value = "" });
+                ViewBag.listarol = listaRol;
+            }
+
+            
+        }
+
+
     }
 }
---
+++ MiPrimerAplicacionWebConEntityFramework/Models/UsuarioCLS.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MiPrimerAplicacionWebConEntityFramework.Models
+{
+    public class UsuarioCLS
+    {
+        public int iidusuario { get; set; }
+        public string nombreusuario { get; set; }
+        public string contra { get; set; }
+        public string iidtipousuario { get; set; }
+        public int iid { get; set; }
+        public int iidrol { get; set; }
+        public int bhabilitado { get; set; }
+
+    }
+}
